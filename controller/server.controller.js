833125027f7420b1348dac65e897cb7f672284e5
Abhishek <abhi.jaiswal1494@gmail.com>
2023-05-21 20:13:31 +0530

    Update server.controller.js

--- controller/server.controller.js
+++ controller/server.controller.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 //----->>>>> Server Contoller <<<<<-----
 const serverController = (req, res) => {
   try {
-    res.status(200).json({ msg: "Welcome" });
+    res.status(200).json({ msg: "Welcome to Wellnesys Home Route!!!" });
   } catch (error) {
     res.status(404).json({ error: error });
   }
