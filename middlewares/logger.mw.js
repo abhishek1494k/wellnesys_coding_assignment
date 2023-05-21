bff7ea1e811a0d9518afd9b5eace77c60d20545e
Abhishek <abhi.jaiswal1494@gmail.com>
2023-05-21 18:41:17 +0530

    Minor Changes Completed

--- middlewares/logger.mw.js
+++ middlewares/logger.mw.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const logger = (req, res, next) => {
     let data = ` Time:- ${new Date()} | Method:- ${req.method } | Url: ${req.url } `
       try {
+        
         fs.appendFileSync("./logger.txt", `${data}\n`, "utf8");
         next();
       } catch (err) {
