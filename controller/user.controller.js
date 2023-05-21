a7dd9726361795315b8a7a1885859933d6e1648d
Abhishek <abhi.jaiswal1494@gmail.com>
2023-05-21 17:28:08 +0530

    Users data fetch and post completed, Middlewares for err handling and invalid  routes integrated

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,18 +1,46 @@
-const express=require("express")
+const express = require("express");
 const app = express();
+const fs = require("fs");
 
-const fs=require("fs");
 
-const saveUser = (req,res) =>{
+//------------->>>>> Get Users Data <<<<<----------------
+const getUser = (req, res) => {
+  fs.readFile(`./config/db.json`, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      const users = JSON.parse(data);
+      res.json(users);
+    }
+  });
+};
 
-    res.send({msg:"Save user"});
 
-}
+//------------->>>>> Save Users Data <<<<<----------------
+const saveUser = (req, res) => {
+  let newUser = req.body;
+  let data;
 
-const getUser = (req,res) =>{
+  fs.readFile(`./config/db.json`, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } else {
+      data = JSON.parse(data);
+      data.push(newUser);
+      fs.writeFile("./config/db.json", JSON.stringify(data), "utf8", (err) => {
+        if (err) {
+          console.error("Error writing file:", err);
+          res.status(500).json({ error: "Internal Server Error" });
+        } else {
+          res.status(201).json(newUser);
+        }
+      });
+    }
+  });
+};
 
-    res.send({msg:"Get User"});
 
-}
 
-module.exports = {saveUser, getUser}
+module.exports = { saveUser, getUser };
